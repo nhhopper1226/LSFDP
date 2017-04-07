cbf64d6bffd2ae7d2902599d8c1019e7473669e0
nhhopper1226 <nhhopper1226@lsr7.net>
2017-04-07 13:08:46 -0500

    Update BaseClass.java

--- FireDepartmentProject/src/firedepartmentproject/BaseClass.java
+++ FireDepartmentProject/src/firedepartmentproject/BaseClass.java
@@ -1,21 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package firedepartmentproject;
+//package firedepartmentproject;
 
-/**
- *
- * @author bsriley0903
- */
-public class BaseClass {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class BaseClass
+{
     
 }
