dd9a711611037789633b49039b998b2ec69513ff
nhhopper1226 <nhhopper1226@lsr7.net>
2017-04-07 13:06:45 -0500

    Update Alerts.java

--- FireDepartmentProject/src/firedepartmentproject/Alerts.java
+++ FireDepartmentProject/src/firedepartmentproject/Alerts.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package firedepartmentproject;
+//package firedepartmentproject;
 
-/**
- *
- * @author bsriley0903
- */
-public class Alerts {
+public class Alerts 
+{
     
 }
