c64a5998cb5dd76a213f19f0f947f4dbbfa0ba59
kaosiude11 <kiude0914@lsr7.net>
2017-04-10 12:48:47 -0500

    Update BaseClass.java

--- FireDepartmentProject/src/firedepartmentproject/BaseClass.java
+++ FireDepartmentProject/src/firedepartmentproject/BaseClass.java
@@ -1,6 +1,13 @@
 //package firedepartmentproject;
+import javax.swing.JFrame;
 
 public class BaseClass
 {
-    
+    public static void main(String[] args)
+    {
+        LabelFrame labelFrame = new LabelFrame;
+        labelFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        labelFrame.setSize(260, 180);
+        labelFrame.setVisible(true);
+    }
 }
