d083444aedbccc4a9060a5387fe79b573e2d6c82
bsriley0903 <bsriley0903@lsr7.net>
2017-04-12 12:21:17 -0500

    Update FireDepartmentDrver.java

--- FireDepartmentProject/src/firedepartmentproject/FireDepartmentDrver.java
+++ FireDepartmentProject/src/firedepartmentproject/FireDepartmentDrver.java
@@ -2,5 +2,10 @@
 
 public class FireDepartmentDrver
 {
-    
+        public static void main(String[] args) {
+        BaseClass jTabbedPaneFrame = new BaseClass();
+        jTabbedPaneFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jTabbedPaneFrame.setSize(1000,900);
+        jTabbedPaneFrame.setVisible(true);
+        }
 }
