74887b5b069a06533f53926f80cfe18608c8bf68
nhhopper1226 <nhhopper1226@lsr7.net>
2017-04-12 13:42:45 -0500

    Create LoginCheck.java

---
+++ FireDepartmentProject/src/firedepartmentproject/LoginCheck.java
@@ -0,0 +1,39 @@
+import javax.swing.JOptionPane;
+
+public class LoginCheck 
+{
+    public static void main(String[] args)
+    {
+        int loop = 0;
+        while (loop != 1)
+        {
+            String username = JOptionPane.showInputDialog("Please input the username.");
+            if (username.equals("FireChief"))
+            {
+                while (loop != 1)
+                {
+                String password = JOptionPane.showInputDialog("Please input the password that goes with \"FireChief\".");
+                if (!password.equals("smokejumper"))
+                    JOptionPane.showMessageDialog(null, "Password associated with \"FireChief\" is incorrect.", "ERROR", JOptionPane.ERROR_MESSAGE);
+                else
+                    loop = 1;
+                }
+            }
+            else if (username.equals("Firefighter"))
+            {
+                while (loop != 1)
+                {
+                    String password = JOptionPane.showInputDialog("Please input the password that goes with \"Firefighter\".");
+                    if (!password.equals("laddermen"))
+                        JOptionPane.showMessageDialog(null, "Password associated with \"Firefighter\" is incorrect.", "ERROR", JOptionPane.ERROR_MESSAGE);
+                    else
+                        loop = 1;
+                }
+            }
+            else
+            {
+                JOptionPane.showMessageDialog(null, "Not a valid username.", "ERROR", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    }
+}
